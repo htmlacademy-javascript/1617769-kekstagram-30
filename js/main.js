27cb4e92adb8a2639ade047a61f6b3d7a3a552ed
Рустам <imamiev@mail.ru>
2023-11-01 22:39:30 +0900

    Рефакторинг thumbnail

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ import './util.js';
 import './data.js';
 import './thumbnail.js';
 import {getPhotoDescriptions} from './data.js';
+import {renderThumbnails} from './thumbnail.js';
+
+renderThumbnails(getPhotoDescriptions());
 window.console.log(getPhotoDescriptions());
 
