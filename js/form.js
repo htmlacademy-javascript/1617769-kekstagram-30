759674f9b2650129b19ed0cd7292107fa794b0e1
Рустам <imamiev@mail.ru>
2023-11-13 19:37:43 +0900

    Правда или действие

--- js/form.js
+++ js/form.js
@@ -1,16 +1,20 @@
+import {isEscapeKey} from './util.js';
+
 const MAX_HASHTAG_COUNT = 5;
+const MAX_COMMENT_COUNT = 140;
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const ErrorText = {
   INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хэштегов`,
   NOT_UNIQUE: 'Хэштеги должны быть уникальными',
   INVALID_PATTERN: 'Неправильный хэштег',
+  INVALID_COMMENT_COUNT: `Длина комментария больше ${MAX_COMMENT_COUNT} символов`,
 };
 
 const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
-const overlay = form.querySelector('.img-upload__overlay');
-const cancelButton = form.querySelector('.img-upload__cancel');
-const fileField = form.querySelector('.img-upload__input');
+const overlay = form.querySelector('.img-upload__overlay');//Форма редактирования изображения
+const cancelButton = form.querySelector('.img-upload__cancel');//Кнопка для закрытия формы редактирования изображения
+const fileField = form.querySelector('.img-upload__input');//
 const hashtagField = form.querySelector('.text__hashtags');
 const commentField = form.querySelector('.text__description');
 
@@ -20,11 +24,9 @@ const pristine = new Pristine(form, {
   errorTextClass: 'img-upload__field-wrapper__error',
 });
 
-const showModal = () => {
-  overlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onDocumentKeydown);
-};
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
 
 const hideModal = () => {
   form.reset();
@@ -34,67 +36,103 @@ const hideModal = () => {
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
-const isTextFieldFocused = () =>
-  document.activeElement === hashtagField ||
-  document.activeElement === commentField;
-
-const normalizeTags = (tagString) => tagString
-  .trim()
-  .split(' ')
-  .filter((tag) => Boolean(tag.length));
-
-const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
-
-const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
-
-const hasUniqueTags = (value) => {
-  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set(lowerCaseTags).site;
-};
-
 function onDocumentKeydown(evt) {
-  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+  if (isEscapeKey(evt) && !isTextFieldFocused()) {
     evt.preventDefault();
     hideModal();
   }
 }
 
-const onCancelButtonClick = () => {
-  hideModal();
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
 const onFileInputChange = () => {
   showModal();
 };
 
-const onFormSubmit = (evt) => {
-  evt.preventDefault();
-  pristine.validate();
+const onCancelButtonClick = () => {
+  hideModal();
 };
+
+const validateComment = (value) => {
+  if(value.length <= 140) {
+    return true;
+  }
+  return false;
+};
+
+const normalizeTags = (tagString) => tagString
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
+
+const turnArrayHashtags = () => hashtagField.value.split(' ');
+
+const hasUniqueTags = () => {
+  const arrayHashtags = turnArrayHashtags();
+  const newArrayHashtags = [];
+  let uniq = true;
+  arrayHashtags.forEach((element) => {
+    if (newArrayHashtags.indexOf(element.toLowerCase()) !== -1) {
+      uniq = false;
+    } else {
+      newArrayHashtags.push(element.toLowerCase());
+    }
+  });
+  return uniq;
+};
+
+const onFormSubmit = (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+};
+
+pristine.addValidator(
+  commentField,
+  validateComment,
+  ErrorText.INVALID_COMMENT_COUNT,
+  1,
+  false
+);
 
 pristine.addValidator(
   hashtagField,
   hasValidCount,
   ErrorText.INVALID_COUNT,
   3,
-  true
+  false
 );
-pristine.addValidator(
-  hashtagField,
-  hasUniqueTags,
-  ErrorText.NOT_UNIQUE,
-  2,
-  true
-);
+
 pristine.addValidator(
   hashtagField,
   hasValidTags,
   ErrorText.INVALID_PATTERN,
   1,
-  true
+  false
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  ErrorText.NOT_UNIQUE,
+  2,
+  false
 );
 
 fileField.addEventListener('change', onFileInputChange);
 cancelButton.addEventListener('click', onCancelButtonClick);
 form.addEventListener('submit', onFormSubmit);
 
+
+// const hasUniqueTags = (value) => {
+//   const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+//   return lowerCaseTags.length === new Set(lowerCaseTags).site;
+// };
