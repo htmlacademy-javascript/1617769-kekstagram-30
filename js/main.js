d59b15c2d127f58c96091bea8c0787819888b0f6
Рустам <imamiev@mail.ru>
2023-11-25 22:02:14 +0900

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 import './util.js';
-import './data.js';
 import './thumbnail.js';
 import './form.js';
 import './gallery.js';
-
-import {getPhotoDescriptions} from './data.js';
 import { renderGallery } from './gallery.js';
-renderGallery(getPhotoDescriptions());
-window.console.log(getPhotoDescriptions());
+import { loadPictures } from './api.js';
+import { showErrorMessage } from './util.js';
+
+async function bootstrap() {
+  try {
+    const pictures = await loadPictures();
+    renderGallery(pictures);
+  } catch {
+    showErrorMessage();
+  }
+}
+
+bootstrap();
